a2ff6842fa13c88a453285f0f12828dfacda46fd
Dila Qonita Hidayati <dilaqonita@apps.ipb.ac.id>
2026-06-02 16:38:32 +0700

    fix: bersihkan pemanggilan aset funpark yang bikin crash

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,11 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<meta name="walkin-store-url"
-      content="{{ route('walkin.store') }}">
+<meta name="walkin-store-url" content="{{ route('walkin.store') }}">
 <title>PlayZone – Sistem Kasir</title>
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-<link rel="stylesheet" href="{{ asset('css/funpark.css') }}">
+{{-- Baris funpark.css sudah dihapus dari sini --}}
 </head>
 <body>
 
@@ -32,6 +31,6 @@
 @include('components.modals')
 
 @stack('scripts')
-<script src="{{ asset('js/funpark.js') }}"></script>
+{{-- Baris funpark.js sudah dihapus dari sini --}}
 </body>
 </html>
